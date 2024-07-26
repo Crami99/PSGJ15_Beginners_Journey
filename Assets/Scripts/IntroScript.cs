883f7d8fd66a8fa4936bb14ba84f05974121c4ad
Osama Hussein <osamahusseinn2@gmail.com>
2024-07-25 23:59:27 -0400

    Fixed the intro video issue when it didn't work in WebGL build

--- Assets/Scripts/IntroScript.cs
+++ Assets/Scripts/IntroScript.cs
@@ -9,11 +9,18 @@ public class IntroScript : MonoBehaviour
     // Reference to the intro video object
     private VideoPlayer introVideo;
 
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
         // Find the intro video object first
         introVideo = GameObject.Find("Intro").GetComponent<VideoPlayer>();
+
+        timer = 0.0f;
+
+        // Find the intro video URL at the StreamingAssets path
+        introVideo.url = System.IO.Path.Combine(Application.streamingAssetsPath, "The Beginner's Journey Intro.mp4");
 
         // Play the intro video at the start as soon as the object is found
         introVideo.Play();
@@ -22,8 +29,10 @@ public class IntroScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // IF the intro video is not playing, transition to the main menu
-        if (!introVideo.isPlaying)
+        timer += Time.deltaTime;
+
+        // If the timer is greater than or equal to 8, transition to the main menu
+        if (timer >= 8.0f)
         {
             SceneManager.LoadScene("MainMenu");
         }
